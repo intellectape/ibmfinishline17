186b1a8a33f8b5e86433a18b664a7c6346c843bc
Aditya <abhardw2@ncsu.edu>
2017-11-07 16:01:11 -0500

    Adding the training data.

--- classifier.py
+++ classifier.py
@@ -1,28 +1,25 @@
 import json
 # from os.path import join, dirname
 from watson_developer_cloud import NaturalLanguageClassifierV1
+import credentials
 
 natural_language_classifier = NaturalLanguageClassifierV1(
-    username='001772a7-7df5-4626-af10-aaec2473203f',
-    password='mMytCmdBIg2Q')
+    username= credentials.USER_ID,
+    password= credentials.PASSWORD)
 
-classifiers = natural_language_classifier.list()
-print(json.dumps(classifiers, indent=2))
-
-
-# create a classifier
-with open('../resources/weather_data_train.csv', 'rb') as training_data:
-     print(json.dumps(natural_language_classifier.create(training_data=training_data, name='weather'), indent=2))
 
 # replace 2374f9x68-nlc-2697 with your classifier id
-status = natural_language_classifier.status('2374f9x68-nlc-2697')
+status = natural_language_classifier.status(credentials.classifier_id)
 print(json.dumps(status, indent=2))
 
 if status['status'] == 'Available':
-    classes = natural_language_classifier.classify('2374f9x68-nlc-2697',
-                                                   'How hot will it be '
-                                                   'tomorrow?')
-    print(json.dumps(classes, indent=2))
+    with open('./trainingData.csv', 'rb') as training_data:
+        for row in training_data:
+            row = row.split(',')
+            row[0] = row[0].strip()
+            classes = natural_language_classifier.classify(credentials.classifier_id, row[0])
+            assert str(classes["top_class"]) == str(row[1]).strip()
+            #print(json.dumps(classes, indent=2))
 
 # delete = natural_language_classifier.remove('2374f9x68-nlc-2697')
 # print(json.dumps(delete, indent=2))
--- createClassifier.py
+++ createClassifier.py
@@ -2,6 +2,14 @@ import json
 # from os.path import join, dirname
 from watson_developer_cloud import NaturalLanguageClassifierV1
 
+natural_language_classifier = NaturalLanguageClassifierV1(
+    username='001772a7-7df5-4626-af10-aaec2473203f',
+    password='mMytCmdBIg2Q')
+
+classifiers = natural_language_classifier.list()
+print(json.dumps(classifiers, indent=2))
+
+
 # create a classifier
-with open('../resources/weather_data_train.csv', 'rb') as training_data:
+with open('./trainingData.csv', 'rb') as training_data:
      print(json.dumps(natural_language_classifier.create(training_data=training_data, name='weather'), indent=2))
